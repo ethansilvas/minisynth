86bfb64965863e748d0077495d2814fe84043ad0
Sherman <ethansilvas2@gmail.com>
2018-07-17 13:14:35 -0700

    design code

---
+++ src/Synth/Synth.js
@@ -0,0 +1,33 @@
+<div class="container">
+  <div class="synth">
+    <div class="settings">
+
+    </div>
+    <div class="keys">
+      <div id="C4" className="white"></div>
+      <div id="Db4" className="black"></div>
+      <div id="D4" className="white"></div>
+      <div id="Eb4" className="black"></div>
+      <div id="E4" className="white"></div>
+      <div id="F4" className="white"></div>
+      <div id="Gb4" className="black"></div>
+      <div id="G4" className="white"></div>
+      <div id="Ab4" className="black"></div>
+      <div id="A4" className="white"></div>
+      <div id="Bb4" className="black"></div>
+      <div id="B4" className="white"></div>
+      <div id="C5" className="white"></div>
+      <div id="Db5" className="black"></div>
+      <div id="D5" className="white"></div>
+      <div id="Eb5" className="black"></div>
+      <div id="E5" className="white"></div>
+      <div id="F5" className="white"></div>
+      <div id="Gb5" className="black"></div>
+      <div id="G5" className="white"></div>
+      <div id="Ab5" className="black"></div>
+      <div id="A5" className="white"></div>
+      <div id="Bb5" className="black"></div>
+      <div id="B5" className="white"></div>
+    </div>
+  </div>
+</div>
